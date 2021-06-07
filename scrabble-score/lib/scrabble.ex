a3d1880b927312c85478502031886cea9b44bd87
Ahljun Ciar <ahljun.ciar@gmail.com>
2021-06-07 10:39:02 +1000

    complete scrabble

---
+++ scrabble-score/lib/scrabble.ex
@@ -0,0 +1,25 @@
+defmodule Scrabble do
+  @doc """
+  Calculate the scrabble score for the word.
+  """
+  @spec score(String.t()) :: non_neg_integer
+  def score(word) do
+    score_map = %{
+      "\t" => 0, "\n" => 0, " " => 0,
+      "A" => 1, "E" => 1, "I" => 1, "O" => 1, "U" => 1, "L" => 1, "N" => 1, "R" => 1, "S" => 1, "T" => 1,
+      "D" => 2, "G" => 2,
+      "B" => 3, "C" => 3, "M" => 3, "P" => 3,
+      "F" => 4, "H" => 4, "V" => 4, "W" => 4, "Y" => 4,
+      "K" => 5,
+      "J" => 8, "X" => 8,
+      "Q" => 10, "Z" => 10,
+    }
+
+    String.upcase(word)
+    |> String.split("", trim: true)
+    |> Enum.reduce(0, fn letter, acc ->
+       acc + Map.get(score_map, letter)
+    end)
+
+  end
+end
