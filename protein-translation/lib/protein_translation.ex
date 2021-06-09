ce0e1ce0e85ede45b8d63b4c236ab53e0fc58485
Ahljun Ciar <ahljun.ciar@gmail.com>
2021-06-09 14:10:58 +1000

    translation

---
+++ protein-translation/lib/protein_translation.ex
@@ -0,0 +1,65 @@
+defmodule ProteinTranslation do
+  @codon_protein_map %{
+    "AUG" => "Methionine",
+    "UUU" => "Phenylalanine", "UUC" => "Phenylalanine",
+    "UUA" => "Leucine", "UUG" => "Leucine",
+    "UCU" => "Serine", "UCC" => "Serine", "UCA" => "Serine", "UCG" => "Serine",
+    "UAU" => "Tyrosine", "UAC" => "Tyrosine",
+    "UGU" => "Cysteine", "UGC" => "Cysteine",
+    "UGG" => "Tryptophan",
+    "UAA" => "STOP", "UAG" => "STOP", "UGA" => "STOP",
+  }
+
+  @doc """
+  Given an RNA string, return a list of proteins specified by codons, in order.
+  """
+  @spec of_rna(String.t()) :: {atom, list(String.t())}
+  def of_rna(rna) do
+    translate = String.codepoints(rna)
+    |> IO.inspect()
+    |> Enum.chunk_every(3)
+    |> Enum.map(&Enum.join/1)
+    |> Enum.reduce_while([], fn codon, acc ->
+      {status, message} = of_codon(codon)
+      if message !== "STOP", do: {:cont, acc ++ [{status, message}]}, else: {:halt, acc}
+    end)
+
+    if Enum.find(translate, fn {status, _codon} -> status == :error end) do
+      {:error, "invalid RNA"}
+    else
+      {:ok, Enum.reduce(translate, [], fn {_status, codon}, acc ->
+        acc ++ [codon]
+      end)}
+    end
+  end
+
+  @doc """
+  Given a codon, return the corresponding protein
+
+  UGU -> Cysteine
+  UGC -> Cysteine
+  UUA -> Leucine
+  UUG -> Leucine
+  AUG -> Methionine
+  UUU -> Phenylalanine
+  UUC -> Phenylalanine
+  UCU -> Serine
+  UCC -> Serine
+  UCA -> Serine
+  UCG -> Serine
+  UGG -> Tryptophan
+  UAU -> Tyrosine
+  UAC -> Tyrosine
+  UAA -> STOP
+  UAG -> STOP
+  UGA -> STOP
+  """
+  @spec of_codon(String.t()) :: {atom, String.t()}
+  def of_codon(codon) do
+    if Map.has_key?(@codon_protein_map, codon) do
+      {:ok, Map.get(@codon_protein_map, codon)}
+    else
+      {:error, "invalid codon"}
+    end
+  end
+end
